96ea584c576488ef4a748eeed6ecd2328b6bb385
Abigail Joseph <ajoseph@Abigails-MacBook-Pro-5.local>
2019-02-28 23:27:05 -0500

    still part2 color.mli changes

--- color.mli
+++ color.mli
@@ -1,11 +1,14 @@
-module type COLOR = 
-	sig 
-		type color
-		type color_name
-		val to_color : int -> int -> int -> color
-		val red : color -> int
-		val green : color -> int
-		val blue : color -> int
-		val color_named : color_name -> color
-	end
-;;
+ 
+type color 
+
+type color_name =
+  | Red | Green | Blue
+  | Orange | Yellow | Indigo | Violet ;;
+
+val to_color : int -> int -> int -> color ;;
+
+val red : color -> int ;;
+val green : color -> int ;;
+val blue : color -> int ;;
+
+val color_named : color_name -> color ;;
