d10e4bd628b91a50b7645d27a16d10e1e6f613fc
Abigail Joseph <ajoseph@Abigails-MacBook-Pro-5.local>
2019-02-28 19:17:12 -0500

    part 2 done

--- lab7_part2.ml
+++ lab7_part2.ml
@@ -70,8 +70,9 @@ below.
 Exercise 2A: Extract the red channel of the color named Red, naming
 the result "red_channel".
 ......................................................................*)
+open Color ;;
 
-let red_channel : int = 0 ;;
+let red_channel : int = red (color_name Red) ;;
 
 (* We hope you'll find the module system quite useful, once you get
 the hang of the conventions.
