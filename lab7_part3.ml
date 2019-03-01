1fa63672551bd122f8e412d606349b893767225a
Abigail Joseph <ajoseph@Abigails-MacBook-Pro-5.local>
2019-02-28 19:16:24 -0500

    part1 done

--- lab7_part3.ml
+++ lab7_part3.ml
@@ -61,16 +61,20 @@ module IntListStack =
     type stack = int list
 
     (* empty -- An empty stack *)
-    let empty : stack = failwith "not implemented"
+    let empty : stack = []
 
     (* push i s -- Adds an integer element i to the top of stack s *)
-    let push (i : int) (s : stack) : stack = failwith "not implemented"
+    let push (i : int) (s : stack) : stack = [i] @ s
 
     (* top s -- Returns the value of the topmost element on stack s *)
-    let top (s : stack) : int = failwith "not implemented"
+    let top (s : stack) : int = match s with
+                                | [] -> raise (EmptyStack)
+                                | hd :: tl -> hd
 
     (* pop s -- Returns a stack with the topmost element from s removed *)
-    let pop (s : stack) : stack = failwith "not implemented"
+    let pop (s : stack) : stack =  match s with
+                                | [] -> raise (EmptyStack)
+                                | hd :: tl -> tl
   end ;;
 
 (* Now let's use this implementation and consider some implications.
@@ -83,14 +87,17 @@ order.
 ......................................................................*)
 
 let small_stack () : IntListStack.stack =
-  failwith "not implemented" ;;
+  let open IntListStack in 
+  push 1 (push 5 empty) ;;
 
 (*......................................................................
 Exercise 3C: Now, use IntListStack functions to write an expression that
 defines last_el as the value of the topmost element from small_stack.
 ......................................................................*)
 
-let last_el = 0;;
+let last_el = 
+  let open IntListStack in 
+  top small_stack;;
 
 (* Based on our requirements above, what should last_el contain?
 
